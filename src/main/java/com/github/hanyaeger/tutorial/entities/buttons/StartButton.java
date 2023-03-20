c4792e742d8ac04f0c6892f418761e6bc6ba5379
Nusayba-El-Mourabet <nsybelmourabet@gmail.com>
2023-03-20 15:33:29 +0100

    .

--- src/main/java/com/github/hanyaeger/tutorial/entities/buttons/StartButton.java
+++ src/main/java/com/github/hanyaeger/tutorial/entities/buttons/StartButton.java
@@ -3,14 +3,17 @@ package com.github.hanyaeger.tutorial.entities.buttons;
 import com.github.hanyaeger.api.Coordinate2D;
 import com.github.hanyaeger.api.entities.impl.TextEntity;
 import com.github.hanyaeger.api.userinput.MouseButtonPressedListener;
+import com.github.hanyaeger.api.userinput.MouseEnterListener;
+import com.github.hanyaeger.api.userinput.MouseExitListener;
 import com.github.hanyaeger.tutorial.TitleScene;
 import com.github.hanyaeger.tutorial.Waterworld;
+import javafx.scene.Cursor;
 import javafx.scene.input.MouseButton;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 
-public class StartButton extends TextEntity implements MouseButtonPressedListener {
+public class StartButton extends TextEntity implements MouseButtonPressedListener, MouseEnterListener, MouseExitListener {
 
     private final Waterworld waterworld;
 
@@ -25,5 +28,17 @@ public class StartButton extends TextEntity implements MouseButtonPressedListene
     @Override
     public void onMouseButtonPressed(MouseButton mouseButton, Coordinate2D coordinate2D) {
         waterworld.setActiveScene(1); // switch to the GameLevel scene
+    }
+
+    @Override
+    public void onMouseEntered() {
+        setFill(Color.VIOLET);
+        setCursor(Cursor.HAND);
+    }
+
+    @Override
+    public void onMouseExited() {
+        setFill(Color.PURPLE);
+        setCursor(Cursor.DEFAULT);
     }
 }
