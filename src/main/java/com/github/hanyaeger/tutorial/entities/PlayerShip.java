a2c84822110f4a79e01b9725b267541a2a64ca3e
Nusayba-El-Mourabet <nsybelmourabet@gmail.com>
2023-05-09 22:15:37 +0200

    .

--- src/main/java/com/github/hanyaeger/tutorial/entities/PlayerShip.java
+++ src/main/java/com/github/hanyaeger/tutorial/entities/PlayerShip.java
@@ -1,10 +1,50 @@
 package com.github.hanyaeger.tutorial.entities;
+
 import com.github.hanyaeger.api.Coordinate2D;
-import com.github.hanyaeger.api.entities.SceneBorderCrossingWatcher;
+import com.github.hanyaeger.api.Size;
+import com.github.hanyaeger.api.entities.*;
 import com.github.hanyaeger.api.entities.impl.DynamicSpriteEntity;
 import com.github.hanyaeger.api.scenes.SceneBorder;
-public class PlayerShip {
-    public PlayerShip(Coordinate2D location){
-        super("sprites/player spaceship.png",location);
+import com.github.hanyaeger.api.userinput.KeyListener;
+import com.github.hanyaeger.tutorial.SpaceWar;
+import javafx.scene.input.KeyCode;
+
+import java.util.Set;
+
+public class PlayerShip extends DynamicSpriteEntity implements SceneBorderCrossingWatcher, KeyListener, SceneBorderTouchingWatcher {
+    public PlayerShip(Coordinate2D location) {
+        super("sprites/playership.png", location, new Size(80, 80), 1, 1);
+    }
+
+    @Override
+    public void notifyBoundaryCrossing(SceneBorder sceneBorder) {
+
+    }
+
+    @Override
+    public void onPressedKeysChange(Set<KeyCode> pressedKeys) {
+
+        if (pressedKeys.contains(KeyCode.UP)) {
+            setMotion(3, 180d);
+        } else if (pressedKeys.contains(KeyCode.DOWN)) {
+            setMotion(3, 0d);
+        } else if (pressedKeys.isEmpty()) {
+            setSpeed(0);
+        }
+    }
+
+    @Override
+    public void notifyBoundaryTouching(SceneBorder border) {
+        setSpeed(0);
+
+        switch (border) {
+            case TOP:
+                setAnchorLocationY(1);
+                break;
+            case BOTTOM:
+                setAnchorLocationY(getSceneHeight() - getHeight() - 1);
+            default:
+                break;
+        }
     }
 }
--- src/main/java/com/github/hanyaeger/tutorial/scenes/GameLevel.java
+++ src/main/java/com/github/hanyaeger/tutorial/scenes/GameLevel.java
@@ -3,6 +3,7 @@ package com.github.hanyaeger.tutorial.scenes;
 import com.github.hanyaeger.api.Coordinate2D;
 import com.github.hanyaeger.api.scenes.DynamicScene;
 import com.github.hanyaeger.tutorial.entities.EnemyShip;
+import com.github.hanyaeger.tutorial.entities.PlayerShip;
 import com.github.hanyaeger.tutorial.entities.buttons.StartButton;
 
 public class GameLevel extends DynamicScene {
@@ -16,5 +17,7 @@ public class GameLevel extends DynamicScene {
     public void setupEntities() {
         var enemyship = new EnemyShip(new Coordinate2D(getWidth() / 2, getHeight() / 2));
         addEntity(enemyship);
+        var playership = new PlayerShip(new Coordinate2D(0, getHeight() / 2));
+        addEntity(playership);
     }
 }
