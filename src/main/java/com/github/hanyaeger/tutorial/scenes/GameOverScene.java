734de59bcb15be9c761ecee802fc8116730f8f10
Nusayba-El-Mourabet <nsybelmourabet@gmail.com>
2023-05-10 13:01:36 +0200

    .

---
+++ src/main/java/com/github/hanyaeger/tutorial/scenes/GameOverScene.java
@@ -0,0 +1,28 @@
+package com.github.hanyaeger.tutorial.scenes;
+
+import com.github.hanyaeger.api.AnchorPoint;
+import com.github.hanyaeger.api.Coordinate2D;
+import com.github.hanyaeger.api.entities.impl.TextEntity;
+import com.github.hanyaeger.api.scenes.StaticScene;
+import com.github.hanyaeger.tutorial.SpaceWar;
+import com.github.hanyaeger.tutorial.entities.buttons.StartButton;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+
+public class GameOverScene extends StaticScene {
+    private SpaceWar spacewar;
+    public  GameOverScene(SpaceWar spacewar) {
+        this.spacewar = spacewar;
+    }
+    @Override
+    public void setupScene() {
+
+
+    }
+
+    @Override
+    public void setupEntities() {
+
+    }
+}
